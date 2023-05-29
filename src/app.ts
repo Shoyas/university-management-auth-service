05f98cfc9fa4a7717f7f1ab335e5489ce5d01db5
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2023-05-29 11:10:49 +0600

    Testing

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-const app: Application = express();
-const port = 5000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
+           app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello from University Management Database')
 })
 
-export default app;
+export default app
